4b637d18275f6fd5871516f1e5bbe9e87e971287
Sonryu <watsonvilar@gmail.com>
2026-03-26 02:47:33 -0300

    codigo aberto. licença MIT

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+#
+#  Copyright (c) 2026 Ramon Watson de Lima Vilar
+#  Licensed under the MIT License. 
+#  See LICENSE file in the project root for full license information.
+#
+#####||##########||##########||##########||##########||##########||##########||##########||#####
+
 import streamlit as st
 import numpy as np
 import formulas as fm 
