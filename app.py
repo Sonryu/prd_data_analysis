1a90add55f5822c2f98d138d6eee74007f75593c
Sonryu <watsonvilar@gmail.com>
2026-03-27 15:34:13 -0300

    adicionado cabeçalho antes das funções para receber os arquivos

--- app.py
+++ app.py
@@ -4,10 +4,19 @@
 #  See LICENSE file in the project root for full license information.
 #
 #####||##########||##########||##########||##########||##########||##########||##########||#####
-
+import tara as tr
 import streamlit as st
 import numpy as np
 import formulas as fm 
 
-st.title('Data Analisys')
-st.header('Teste estatico')
+# cabeçalho
+# ---------
+st.logo('Logo - PRD.png')
+st.set_page_config(page_title='PRD: Análise de dados de teste estático', layout='centered', page_icon='Logo - PRD.png')
+st.image('elet.jpg')
+st.header('Sistema de análise de dados de teste estático')
+st.subheader('Potiguar Rocket Design', divider=True)
+
+with st.sidebar:
+    st.image('analise_de_dados.jpg')
+
