942cece76fd323b4b5b419a1be1d9949cf843819
Sonryu <watsonvilar@gmail.com>
2026-03-27 16:00:38 -0300

    adicionado função para receber arquivos

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ import formulas as fm
 
 # cabeçalho
 # ---------
+
+# > mudar imagem elet.png
 st.logo('Logo - PRD.png')
 st.set_page_config(page_title='PRD: Análise de dados de teste estático', layout='centered', page_icon='Logo - PRD.png')
 st.image('elet.jpg')
@@ -20,3 +22,11 @@ st.subheader('Potiguar Rocket Design', divider=True)
 with st.sidebar:
     st.image('analise_de_dados.jpg')
 
+# upload de arquivo
+# -----------------
+uploaded_files = st.file_uploader(
+        'Upload de arquivo para análise'
+        ' Arquivos aceitos: `.csv`, `.txt` ou `.wsv` com valores separados por espaço "` `".',
+        accept_multiple_files=True, type=['csv', 'txt', 'wsv'])
+if not uploaded_files:
+    continue
